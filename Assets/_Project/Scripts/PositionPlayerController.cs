e8f1d23d5874f1e7a71b1d4a8e63fac333fe8da9
grammer89 <raffaelescotti89@gmail.com>
2026-02-24 20:55:41 +0100

    modify

--- Assets/_Project/Scripts/PositionPlayerController.cs
+++ Assets/_Project/Scripts/PositionPlayerController.cs
@@ -1,12 +1,20 @@
-using UnityEngine;
 using System;
 using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
 [Serializable]
 public class PositionPlayerController : MonoBehaviour
 {
     [SerializeField] Vector3 _positionPlayer;
     // Start is called before the first frame update
     private string _path;
+    public struct Dir
+    {
+        public float x;
+        public float y;
+        public float z;
+
+    }
 
     private void Awake()
     {
@@ -15,12 +23,12 @@ public class PositionPlayerController : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.L))
         {
             LoadJson();
         }
 
-        if (Input.GetKey(KeyCode.Y))
+        if (Input.GetKeyDown(KeyCode.Y))
         {
             SaveJson();
         }
@@ -28,9 +36,17 @@ public class PositionPlayerController : MonoBehaviour
 
     public void SaveJson()
     {
-
         _positionPlayer = transform.position;
-        string jsonText = JsonUtility.ToJson(_positionPlayer);
+        Dir newDir = new()
+        {
+            x = _positionPlayer.x,
+            y = _positionPlayer.y,
+            z = _positionPlayer.z
+        };
+
+        //string jsonText = JsonUtility.ToJson(_positionPlayer);
+        //string jsonText = JsonConvert.SerializeObject(_positionPlayer);
+        string jsonText = JsonConvert.SerializeObject(newDir);
         File.WriteAllText(_path, jsonText);
         Debug.Log("La posizione " + _positionPlayer + "è stata salvata nel path " + _path);
     }
@@ -38,9 +54,10 @@ public class PositionPlayerController : MonoBehaviour
     public void LoadJson()
     {
         string jsonText = File.ReadAllText(_path);
-        Vector3 data = JsonUtility.FromJson<Vector3>(jsonText);
+        //Vector3 data = JsonUtility.FromJson<Vector3>(jsonText);
+        Vector3 data = JsonConvert.DeserializeObject<Vector3>(jsonText);
         Debug.Log(data);
         transform.position = data;
-        Debug.Log("La posizione " + _positionPlayer + "è stata caricata dal path " + _path);
+        Debug.Log("La posizione " + data + "è stata caricata dal path " + _path);
     }
 }
